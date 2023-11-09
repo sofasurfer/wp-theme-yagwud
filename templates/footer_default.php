fa45fad4d5e0093847c8837ab26a4d19802ae96c
SofaSurfer <kilian@sofasurfer.ch>
2023-11-09 18:08:51 +0100

    Fix footer

--- templates/footer_default.php
+++ templates/footer_default.php
@@ -1,4 +1,4 @@
 <footer class="container">
-    <small>Built by <a href="https://www.sofasurfer.org" title="Sofasurfer.org" target="_blank">sofasurfer.org</a></small>
+    <!--small>Built by <a href="https://www.sofasurfer.org" title="Sofasurfer.org" target="_blank">sofasurfer.org</a></small-->
 </footer>
 
--- templates/radio.php
+++ templates/radio.php
@@ -2,8 +2,9 @@
 $streaminfo = get_query_var( 'get_stream_info' );
 if ( $streaminfo ):
 ?>
-    <div class="col-md-12">
+    <div class="_col-md-12">
         <p>Start: <?= $streaminfo['stream_start']; ?></p>
+        <!--p>Show will start soon</p-->
     </div>
     <div class="col-md-12">
         <audio src="https://stream.yagwud.com:9005/stream" controls ></audio>
@@ -15,6 +16,6 @@ if ( $streaminfo ):
     </div-->
 <?php else: ?>
     <div class="col-md-12">
-        <h3>Stream is offline</h3>
+        <h3>offline :/</h3>
     </div>
 <?php endif; ?>
