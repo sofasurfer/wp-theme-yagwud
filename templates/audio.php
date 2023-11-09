3eaac9a911f225d115a776bb41990e9ed180d92d
sofasurfer <kilian@sofasurfer.ch>
2023-11-09 17:11:51 +0100

    Add audio CSS and list

---
+++ templates/audio.php
@@ -0,0 +1,25 @@
+<div class="container">
+    <div class="row">
+         <div class="col-md-12">
+          <h2>Stream:</h2>
+         </div>
+         [get_radio]
+    </div>
+    <div class="row">
+         <div class="col-md-12">
+          <h2>Archive:</h2>
+         </div>
+    </div>
+    <div class="row">
+        <div class="col-md-2">
+            <img src="https://www.yagwud.com/content/uploads/2023/11/ausio-miro-napoleon.jpeg" class="img-responsive" />
+        </div>
+        <div class="col-md-10">
+        <h3>08.11.2023 - With Napoleon Maddox & Miro Caltagirone</h3>
+        <p>100% vinyl Fantastic journey through the history of music.</p>
+        <audio controls>
+        <source src="https://stream.sofasurfer.org/mp3/yagwud/rec_20231108-220834.mp3" type="audio/mp3">
+        Your browser does not support the audio tag.
+        </audio>    
+        </div>
+</div>
--- templates/radio.php
+++ templates/radio.php
@@ -8,13 +8,8 @@ if ( $streaminfo ):
     <div class="col-md-12">
         <audio src="https://stream.yagwud.com:9005/stream" controls ></audio>
     </div>
-    <!--div class="col-md-6">
-        <p>Problem streaming: <a class="btn btn-success" target="_blank" href="https://radio.odowok.com:9005/stream">click here</a></p>
-
-        <p><a class="btn btn-success btn-large" target="_blank" href="<?= $streaminfo['listenurl']; ?>">PLAY</a></p>
-    </div-->
 <?php else: ?>
     <div class="col-md-12">
-        <h3>Stream is offline</h3>
+        <h3>Stream is offline :(</h3>
     </div>
 <?php endif; ?>
