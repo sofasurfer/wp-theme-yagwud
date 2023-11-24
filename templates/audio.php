faa290f2bbb138bfa9e420f871b559b0fdc0e322
sofasurfer <kilian@sofasurfer.ch>
2023-11-24 16:47:04 +0100

    Update audio

--- templates/audio.php
+++ templates/audio.php
@@ -1,25 +1,43 @@
-<div class="container">
-    <div class="row">
-         <div class="col-md-12">
-          <h2>Stream:</h2>
-         </div>
-         [get_radio]
+<div class="main-content container">
+  <div class="row">
+    <!-- Use Bootstrap 5 utility classes for spacing (mb-3 for margin-bottom) -->
+    <div class="mb-3">
+      <!-- Assuming [get_radio] generates content -->
+      [get_radio]
     </div>
-    <div class="row">
-         <div class="col-md-12">
-          <h2>Archive:</h2>
-         </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-2">
+      <h2>Archive:</h2>
     </div>
-    <div class="row">
-        <div class="col-md-2">
-            <img src="https://www.yagwud.com/content/uploads/2023/11/ausio-miro-napoleon.jpeg" class="img-responsive" />
-        </div>
-        <div class="col-md-10">
-        <h3>08.11.2023 - With Napoleon Maddox & Miro Caltagirone</h3>
-        <p>100% vinyl Fantastic journey through the history of music.</p>
-        <audio controls>
+  </div>
+
+  <div class="row">
+    <div class="col-md-2  mb-4 mb-md-0">
+      <img src="https://www.yagwud.com/content/uploads/2023/11/ausio-miro-napoleon.jpeg" class="img-fluid" alt="Responsive image">
+    </div>
+    <div class="col-md-10">
+      <h3>08.11.2023: Napoleon Maddox & Miro Caltagirone</h3>
+      <p>100% vinyl: "It's a beautiful music time travel with two full on professionals."</p>
+      <audio controls>
         <source src="https://stream.sofasurfer.org/mp3/yagwud/rec_20231108-220834.mp3" type="audio/mp3">
         Your browser does not support the audio tag.
-        </audio>    
-        </div>
+      </audio>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-2  mb-4 mb-md-0">
+      <img src="https://www.yagwud.com/content/uploads/2023/11/putsmarie-2001.jpg" class="img-fluid" alt="Responsive image">
+    </div>
+    <div class="col-md-10">
+      <h3>19.10.2001: PutsMarie Live Artix</h3>
+      <p>What a crazy concert, just listen and have fun.</p>
+      <audio controls>
+        <source src="https://stream.sofasurfer.org/mp3/PutsMarieLive90s.mp3" type="audio/mp3">
+        Your browser does not support the audio tag.
+      </audio>
+    </div>
+  </div>
 </div>
