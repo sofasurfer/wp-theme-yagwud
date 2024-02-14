5d50813a98b8b87bb4e43a8aea922d0e6ecf0b40
SofaSurfer <kilian@sofasurfer.ch>
2024-02-14 10:09:41 +0100

    Add bubbles

--- templates/stream.php
+++ templates/stream.php
@@ -3,7 +3,7 @@ $streaminfo = get_query_var( 'get_stream_info' );
 ?>
 <div class="row">
     <div class="col-md-2 mb-4 mb-md-0">
-        <a href="<?= get_field('stream_url',$streaminfo);?>" target="_blank"><img class="img-fluid" src="<?= get_the_post_thumbnail_url($streaminfo,'large');?>" alt="Responsive image" /></a>
+        <a href="<?= get_the_post_thumbnail_url($streaminfo,'full');?>" target="_blank"><img class="img-fluid" src="<?= get_the_post_thumbnail_url($streaminfo,'large');?>" alt="Responsive image" /></a>
     </div>
     <div class="col-md-10">
         <h3><?= get_the_title($streaminfo);?></h3>
@@ -13,7 +13,7 @@ $streaminfo = get_query_var( 'get_stream_info' );
         <source src="<?= get_field('stream_url',$streaminfo);?>" type="audio/mp3" />
         Your browser does not support the audio tag.
         </audio>
-        <p><?= get_the_date('d. M Y',$streaminfo);?></p>
+        <p><a href="<?= get_field('stream_url',$streaminfo);?>" target="_blank"><?= get_the_date('d. M Y',$streaminfo);?></a></p>
     </div>
     <div class="col-md-12">
         <hr/>
