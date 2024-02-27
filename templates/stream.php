44ae8347941c3de36bb28374fb73ceddca6d05a8
SofaSurfer <kilian@sofasurfer.ch>
2024-02-27 18:52:42 +0100

    Update stream

--- templates/stream.php
+++ templates/stream.php
@@ -6,7 +6,7 @@ $streaminfo = get_query_var( 'get_stream_info' );
         <a href="<?= get_the_post_thumbnail_url($streaminfo,'full');?>" target="_blank"><img class="img-fluid" src="<?= get_the_post_thumbnail_url($streaminfo,'large');?>" alt="Responsive image" /></a>
     </div>
     <div class="col-md-8">
-        <p><a class="text-meta" href="<?= get_field('stream_url',$streaminfo);?>" target="_blank"><?= get_the_date('d. M Y',$streaminfo);?></a></p>
+        <a class="text-meta" href="<?= get_field('stream_url',$streaminfo);?>" target="_blank"><?= get_the_date('d. M Y',$streaminfo);?></a>
         <h3><?= get_the_title($streaminfo);?></h3>
         <p><strong><?= get_field('description',$streaminfo);?></strong></p>
 
